9d48493b6780b0f7d203d7e7a2c9c3fbe1093599
kim.ranim <evgeniy.polikarpov95@gmail.com>
2018-11-07 04:22:17 +0300

    asd

--- Modeling/MainWindow.xaml.cs
+++ Modeling/MainWindow.xaml.cs
@@ -17,23 +17,25 @@ namespace Modeling
 	{
         private const string GROUP_BOX = "TextBox";
         private const string GRID = "Grid";
-        private TextBlock[,] groupBox;
+        private Canvas[,] groupBox;
 
-        private const double SIZE_ROW = 100;
+        private const double IMAGE_SIZE = 10;
+        private const double SIZE_ROW = 70;
         private const int HEIGHT = 10;
         private const int WIDHT = 10;
         private BrushConverter conv = new BrushConverter();
         private Island island;
 
-        private Grid grid;
+        private Canvas grid;
+        private Canvas panel;
 
         public MainWindow()
 		{
 			InitializeComponent();
             island = new Island(HEIGHT, WIDHT);
-            grid = this.FindName(GRID) as Grid;
+            panel =  this.FindName("Panel") as Canvas;
+            grid = panel.FindName(GRID) as Canvas;
             GenerateRows();
-            
             
         }
 
@@ -45,19 +47,22 @@ namespace Modeling
 
         public void GenerateRows()
         {
-            var groupBoxes = new TextBlock[HEIGHT, WIDHT];
+            var groupBoxes = new Canvas[HEIGHT, WIDHT];
             for (int i = 0; i != HEIGHT; ++i)
             {
                 for (int j = 0; j != WIDHT; ++j)
                 {
-                    var cloneGroupBox = new TextBlock();
+                    var cloneGroupBox = new Canvas();
+                    
+
                     cloneGroupBox.Width = SIZE_ROW;
                     cloneGroupBox.Height = SIZE_ROW; 
                     cloneGroupBox.Name = $"{GROUP_BOX}{i}_{j}";
                     cloneGroupBox.Margin = new Thickness(i * SIZE_ROW, j * SIZE_ROW, 0, 0);
-                    grid.Children.Add(cloneGroupBox);
-                    groupBoxes[i, j] = cloneGroupBox;
+                    panel.Children.Add(cloneGroupBox);
 
+                    groupBoxes[i, j] = cloneGroupBox;
+                    
                     UpdateCell(island.Cells[i, j], cloneGroupBox);
                 }
             }
@@ -76,7 +81,7 @@ namespace Modeling
             }
         }
 
-        private void ChangeCellColor(ICell cell, TextBlock groupBox)
+        private void ChangeCellColor(ICell cell, Canvas groupBox)
         {
             if (cell.GetLocality() == Locality.Field)
             {
@@ -110,25 +115,61 @@ namespace Modeling
             }
         }
 
-        private void UpdateCell(ICell cell, TextBlock groupBox)
+        private void UpdateCell(ICell cell, Canvas groupBox)
         {
             ChangeCellColor(cell, groupBox);
             UpdateText(cell, groupBox);
         }
 
-        private void UpdateText(ICell cell, TextBlock groupBox)
+        private void UpdateText(ICell cell, Canvas groupBox)
         {
             if (cell.GetLocality() == Locality.Field)
             {
-                groupBox.Text = $"R : {cell.GetRubbits()}{Environment.NewLine}H : {cell.GetHunters()}{Environment.NewLine}W : {cell.GetWolfs()}{Environment.NewLine}";
+                groupBox.Children.Clear();
+
+                for (int i = 0; i != cell.GetRubbits(); ++i)
+                {
+                    var image = new Canvas();
+                    image.Margin = new Thickness(i * IMAGE_SIZE * 2 + IMAGE_SIZE, IMAGE_SIZE, 0, 0);
+                    image.Height = IMAGE_SIZE;
+                    image.Width = IMAGE_SIZE;
+                    image.Background = Brushes.White;
+                    groupBox.Children.Add(image);
+                }
+
+                for (int i = 0; i != cell.GetHunters(); ++i)
+                {
+                    var image = new Canvas();
+                    image.Margin = new Thickness(i * IMAGE_SIZE * 2 + IMAGE_SIZE, IMAGE_SIZE * 3, 0, 0);
+                    image.Height = IMAGE_SIZE;
+                    image.Width = IMAGE_SIZE;
+                    image.Background = Brushes.Brown;
+                    groupBox.Children.Add(image);
+                }
+
+                for (int i = 0; i != cell.GetWolfs(); ++i)
+                {
+                    var image = new Canvas();
+                    image.Margin = new Thickness(i * IMAGE_SIZE * 2 + IMAGE_SIZE, IMAGE_SIZE * 5, 0, 0);
+                    image.Height = IMAGE_SIZE;
+                    image.Width = IMAGE_SIZE;
+                    image.Background = Brushes.Red;
+                    groupBox.Children.Add(image);
+                }
+
+                //image.Background
+
+
+                //var textBlock = groupBox.Children[0] as TextBlock;
+                //textBlock.Text  = $"Rubbits : {cell.GetRubbits()}{Environment.NewLine}Hunters : {cell.GetHunters()}{Environment.NewLine}Wolfs : {cell.GetWolfs()}{Environment.NewLine}";
             }
         }
 
-        private TextBlock Load(string cloneName)
+        private Canvas Load(string cloneName)
         {
             StringReader stringReader = new StringReader(cloneName);
             XmlReader xmlReader = XmlReader.Create(stringReader);
-            return XamlReader.Load(xmlReader) as TextBlock;
+            return XamlReader.Load(xmlReader) as Canvas;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
